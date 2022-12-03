4053ae104ee787853f43979ca9b74ac365e13604
Hariyansyah99 <m.a.hariyansyah@gmail.com>
2022-12-03 10:54:46 +0700

    Menambahkan grafik temp dan pressure

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ async function getdata() {
     );
 
     const data = await res.json();
+    if (data.error != null) {
+        alert(data.error.message);
+      }
     document.getElementById("location").innerText = data.location.name;
     document.getElementById("locationParts").innerHTML = "<i class='bi bi-geo-alt'></i> " + data.location.region + " , " + data.location.country;
     document.getElementById("dateTime").innerHTML = "<i class='bi bi-calendar'></i> " + data.forecast.forecastday[length].date;
@@ -24,5 +27,95 @@ async function getdata() {
     document.getElementById("max-temp").innerText = data.forecast.forecastday[length].day.maxtemp_c  + " °C";
     document.getElementById("max-wind").innerText = data.forecast.forecastday[length].day.maxwind_kph + "km/h";
     document.getElementById("weatherIcon").src ="https:" + data.forecast.forecastday[length].day.condition.icon ;
+
+    const forecastDay = data.forecast.forecastday[0];
+    const tempCont = document.getElementById('tempCont');
+    const pressureCont = document.getElementById('pressureCont');
+
+    document.getElementById('chartTemp').remove();
+    document.getElementById('chartPressure').remove();
+
+    tempCont.innerHTML += "<canvas id='chartTemp' ></canvas>";
+    pressureCont.innerHTML += "<canvas id='chartPressure' ></canvas>";
+
+    const ctxTemp = document.getElementById('chartTemp');
+    const ctxPressure = document.getElementById('chartPressure');
+
+    let labelHours = [];
+    let dataTemp = [];
+    let dataPressure = [];
+
+    for (let i = 0; i < 24; i++) {
+        let teks = forecastDay.hour[i].time.split(' ');
+        teks = teks[1];
+        labelHours.push(teks);
+      }
+
+      for (let i = 0; i < forecastDay.hour.length; i++) {
+        dataTemp.push(forecastDay.hour[i].temp_c);
+        dataPressure.push(forecastDay.hour[i].pressure_mb);
+      }
+
+
+      const grTemp = new Chart(ctxTemp, {
+        type: 'line',
+        data: {
+          labels: labelHours,
+          datasets: [
+            {
+              label: 'Temperature (°C)',
+              data: dataTemp,
+              borderWidth: 1,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                callback: function (value, index, ticks) {
+                  return value + ' °C';
+                },
+              },
+            },
+          },
+        },
+      });
+
+      const grPressure = new Chart(ctxPressure, {
+        type: 'line',
+        data: {
+          labels: labelHours,
+          datasets: [
+            {
+              label: 'Pressure (Milibar)',
+              data: dataPressure,
+              borderWidth: 1,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                callback: function (value, index, ticks) {
+                  return value + ' mb';
+                },
+                stepSize: 0.1,
+              },
+              min: Math.min(...dataPressure) - 1,
+            },
+          },
+        },
+      });
+
 }
 
